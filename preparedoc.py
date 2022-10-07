ba1f38390056835a12b8b7d22e0acac4db4f8111
MinunJuttu <115250641+MinunJuttu@users.noreply.github.com>
2022-10-07 17:41:20 +0300

    Update preparedoc.py

--- preparedoc.py
+++ preparedoc.py
@@ -2,7 +2,7 @@ import nltk
 import re
 import string
 
-
+#функция для токенизации текста
 def wordtoken(text):
     text = text.read()
     text = text.lower() 
@@ -10,6 +10,7 @@ def wordtoken(text):
     word_token = nltk.word_tokenize(text)
     return word_token
 
+#функция для удаления стопслов (фильтрация) и сохранения словаря
 def filterednsave(word_token):
     stop_words = nltk.corpus.stopwords.words('russian')
     filtered = [word for word in word_token if word not in stop_words]
@@ -19,6 +20,7 @@ def filterednsave(word_token):
         dictionary.write(word + '\n')
     dictionary.close()
 
+#собсна, исполняемый код: открытие файла, функции, закрытие
 text = open('testfortest.txt') 
 word_token = wordtoken(text)
 word_token = filterednsave(word_token)
