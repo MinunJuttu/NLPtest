7e6cb448760c0d4246b5883e138e33e84a7fbe5b
MinunJuttu <115250641+MinunJuttu@users.noreply.github.com>
2022-10-07 17:49:10 +0300

    Update failed_test ONE.py

--- failed_test ONE.py
+++ failed_test ONE.py
@@ -5,7 +5,7 @@ import sklearn
 from sklearn import neighbors
 import numpy as np
 
-
+#подготовка файла, токенизация, фильтрация
 text = open('testfortest.txt')
 text = text.read()
 text = text.lower()
@@ -14,11 +14,12 @@ word_token = nltk.word_tokenize(text)
 stop_words = nltk.corpus.stopwords.words('russian')
 filtered = [word for word in word_token if word not in stop_words]
 
-
+#подготовка для работы с функциями sklearn
 from sklearn.feature_extraction.text import CountVectorizer 
 vectorizer = CountVectorizer(max_features=1500) 
 dox = vectorizer.fit_transform(filtered).toarray()
 
+#великий костыль — введи слово, которое будет сохранено в документе для подготовки работы в sklearn
 request = input('слово: ')
 if request:
     doc = open('doctest.txt', 'w')
@@ -35,8 +36,11 @@ if request:
     from sklearn.neighbors import KNeighborsClassifier
     knn = neighbors.KNeighborsClassifier(n_neighbors = 3)
     knn.fit(dox, word)
+    # тут проводится обучение по основному доку и введёному слову
    
     words = knn.predict(1)
     print(words)
+    #в плане было, что обучившись система начнёт выдавать наиболее близких соседей введённому слову.
+    #я до сих пор думаю, что идея хорошая. но моих нынешних знаний не хватает для её воплощения.
 else:
     pass
